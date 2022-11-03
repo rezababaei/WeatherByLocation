c954efc70e393c420ae7997160f01ea95ec5fbe0
Reza Babaei <rzbabaei1@gmail.com>
2022-11-03 08:43:10 +0100

    recycler view added to home fragment

--- app/src/main/java/com/rezababaei/weatherbylocation/network/ApiRepository.kt
+++ app/src/main/java/com/rezababaei/weatherbylocation/network/ApiRepository.kt
@@ -1,10 +1,15 @@
 package com.rezababaei.weatherbylocation.network
 
+import com.rezababaei.weatherbylocation.BuildConfig
 import com.rezababaei.weatherbylocation.network.datamodel.WeatherResponse
 import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface ApiRepository {
     suspend fun getWeather(lat:String?, lon:String?, callback: RequestCompleteListener<WeatherResponse>)
+
+
 
 }
